1b2f8b336f152967b21b30adc7f669a2718edc01
Antonia Horga <antoniamaria.horga@ulbsibiu.ro>
2026-05-04 19:00:13 +0300

    Merge remote-tracking branch 'origin/master'

--- src/lab8/lab8Main.java
+++ src/lab8/lab8Main.java
@@ -1,5 +1,8 @@
 package lab8;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -35,7 +38,61 @@ public class lab8Main {
             e.printStackTrace();
         }
     }
+
+    public static void copyAndAverage(String input,String output){
+        try(FileInputStream fis =new FileInputStream(input);
+        Workbook workbook=new XSSFWorkbook(fis);
+        Workbook outWorkbook=new XSSFWorkbook()){
+
+            Sheet sheet=workbook.getSheetAt(0);
+            Sheet outSheet=outWorkbook.createSheet();
+
+            int rowNum=0;
+
+            for(Row row:sheet){
+
+                Row newRow=outSheet.createRow(rowNum);
+
+                int colNum=0;
+                int sum=0;
+
+                int lastCol=row.getLastCellNum();
+
+                for(Cell cell:row){
+                    Cell newCell=newRow.createCell(colNum);
+
+                    if(cell.getCellType()==CellType.STRING){
+                        newCell.setCellValue(cell.getStringCellValue());
+                    }
+
+                    if(cell.getCellType()==CellType.NUMERIC){
+                        double val=cell.getNumericCellValue();
+                        newCell.setCellValue(val);
+
+                        if(colNum>=lastCol-3){
+                            sum+=val;
+                        }
+                    }
+                    colNum++;
+                }
+
+                double avg=sum/3;
+                newRow.createCell(colNum).setCellValue(avg);
+                rowNum++;
+            }
+            FileOutputStream fos=new FileOutputStream(output);
+            outWorkbook.write(fos);
+            fos.close();
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
     void main(){
         readExcel("laborator8_input.xlsx");
+
+        copyAndAverage("laborator8_input.xlsx","laborator8_output2.xlsx");
     }
 }
